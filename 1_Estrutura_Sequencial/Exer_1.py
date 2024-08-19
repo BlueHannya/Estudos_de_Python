ca25dfa521c8933a505e3ff20197a80342738262
BlueHannya <alle.foz@hotmail.com>
2024-08-18 23:33:48 -0300

    Upando o resto das coisas em ordem 1.2

--- 1_Estrutura_Sequencial/Exer_1.py
+++ 1_Estrutura_Sequencial/Exer_1.py
@@ -1,3 +1,3 @@
 """Faça um Programa que mostre a mensagem "Alo mundo" na tela."""
 
-print("\033[32mHello fucking World\033[m\n")
+print("\n\033[32mHello fucking World\033[m\n")
--- 1_Estrutura_Sequencial/Exer_4.py
+++ 1_Estrutura_Sequencial/Exer_4.py
@@ -1,16 +1,17 @@
 """Faça um Programa que peça as 4 notas bimestrais e mostre a média."""
 
-num1 = float(input("Informe sua nota do 1 bimestre para calculár-mos sua média: "))
-num2 = float(input("Informe sua nota do 2 bimestre para calculár-mos sua média: "))
-num3 = float(input("Informe sua nota do 3 bimestre para calculár-mos sua média: "))
-num4 = float(input("Informe sua nota do 4 bimestre para calculár-mos sua média: "))
+num1 = float(input("\nInforme sua nota do 1 bimestre para calculár-mos sua média: "))
+num2 = float(input("\nInforme sua nota do 2 bimestre para calculár-mos sua média: "))
+num3 = float(input("\nInforme sua nota do 3 bimestre para calculár-mos sua média: "))
+num4 = float(input("\nInforme sua nota do 4 bimestre para calculár-mos sua média: "))
 
 soma = num1 + num2 + num3 + num4
 media = float(soma/4)
 
-print(f"Suas notas desse ano foram de:\nNo primeiro bimestre {num1:.2f},\nNo segundo bimestre {num2:.2f},\nNo terceiro bimestre {num3:.2f},\nNo quarto bimestre {num4:.2f}.\nSua média anual é de: {media:.2f}")
+print(f"""\nSuas notas desse ano foram de:\n\n- No primeiro bimestre {num1:.2f}\n- No segundo bimestre {num2:.2f}
+- No terceiro bimestre {num3:.2f}\n- No quarto bimestre {num4:.2f}\n\nSua média anual é de: {media:.2f}""")
 
 if media >= 5:
-    print("\nAluno Aprovado!!!")
+    print("\nAluno Aprovado!!!\n")
 else:
-    print("\nAluno Reprovado...")
+    print("\nAluno Reprovado...\n")
